7143d41908be93d26f5d384d4b0e5b4d2369836c
Kacper Niescierowicz <kapoludek@gmail.com>
2019-11-24 19:46:31 +0100

    Temperature Simulator

---
+++ WebApplication14/TemperatureSimulator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHome
+{
+    public static class TemperatureSimulator
+    {
+        public static int GetTemperature
+        {
+            Random rnd = new Random();
+            return (rnd.Next(0,50) - 20)
+        }
+    }
+}
